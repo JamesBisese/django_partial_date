f9fc1e4b5f6c1042a17f720bdbe0a808e1e5d20c
James Bisese <james.bisese@tetratech.com>
2022-03-03 09:48:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,22 +9,22 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="django_partial_date",
-    version="1.3.1",
-    description="Django custom model field for partial dates with the form YYYY, YYYY-MM, YYYY-MM-DD",
+    name="django_partial_datetime",
+    version="1.0.1",
+    description="Django custom model field for partial datetimes with the form YYYY, YYYY-MM, YYYY-MM-DD, YYYY-MM-DD HH(?) and YYYY-MM-DD HH:mm",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ktowen/django_partial_date",
-    author="ktowen",
-    author_email="towenpa@gmail.com",
+    author="jamesbisese",
+    author_email="jimmy@jimmyandjanie.com",
     license="MIT",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 1 - Experimenting",
         "Framework :: Django",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
     ],
-    keywords=["fields", "django", "dates", "partial"],
-    packages=["partial_date"],
+    keywords=["fields", "django", "dates", "datetimes", "partial"],
+    packages=["partial_datetime"],
     install_requires=["six", "django"],
 )
